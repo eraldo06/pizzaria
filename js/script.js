c2fd90428e32175a8ffe315a9d275e7429406af3
eraldo06 <eraldoleonardo384@gmail.com>
2022-11-07 21:00:19 -0300

    colocando os primeiros dados no html

--- js/script.js
+++ js/script.js
@@ -5,5 +5,11 @@ pizzaJson.map( (item, index)=>{
     // clonado uma base html
     let pizzaItem = s('.models .pizza-item').cloneNode(true);
 
+    // colocando dados no html
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
     s('.pizza-area').append(pizzaItem)
 });
