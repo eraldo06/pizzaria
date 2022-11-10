51eb326fe5836aa28fbb3dc92b7ab791cc6d8655
eraldo06 <eraldoleonardo384@gmail.com>
2022-11-09 21:26:17 -0300

    erro corrigido

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-pizzaJson.map( (item, index)=>{
-    const s = (el)=>document.querySelector(el);
-    const sa = (el)=>document.querySelectorAll(el);
+const s = (el)=>document.querySelector(el);
+const sa = (el)=>document.querySelectorAll(el);
 
+pizzaJson.map((item, index)=>{
+   
     // clonado uma base html
     let pizzaItem = s('.models .pizza-item').cloneNode(true);
 
     // colocando dados no html
-    pizzaItem.setAttribute('data-key', index)
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
@@ -14,9 +15,11 @@ pizzaJson.map( (item, index)=>{
 
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
-        let key = e.target.closest('.pizza-item').getAttribute('date-key');
-        console.log(pizzaJson[key]);
-        // s('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        s('.pizzaBig img').src = pizzaJson[key].img;
+       s('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+       s('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
 
 
         s('.pizzaWindowArea').style.opacity = 0;
