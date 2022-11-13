f52f87580f4014ee1ba5c179dd3a10bdae729e49
eraldo06 <eraldoleonardo384@gmail.com>
2022-11-13 15:32:02 -0300

    update carrinho

--- js/script.js
+++ js/script.js
@@ -94,10 +94,32 @@ s('.pizzaInfo--addButton').addEventListener('click', ()=>{
 })
 
 function updateCart(){
-    if(cart.length >0){
+    if(cart.length > 0){
         s('aside').classList.add('show');
+        s('.cart').innerHTML = ''
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item)=>item.id==cart[i].id)
+            let cartItem = s('.models .cart--item').cloneNode(true)
+
+            let pizzaSizeName;
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = 'P'
+                    break
+                case 1:
+                    pizzaSizeName = 'M'
+                    break
+                case 2:
+                    pizzaSizeName = 'G'
+                    break
+            }
+            let pizzaName =`${pizzaItem.name} (${pizzaSizeName
+            })`
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+
+            s('.cart').append(cartItem);
         }
     }else{
         s('aside').classList.remove('show');
